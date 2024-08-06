ec746cd1023fd725758292a53cc23ccca667156e
Daniel Berthran <berthlearn@gmail.com>
2024-08-06 02:59:33 +0100

    Arrange imports in alphabetical order

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-'''
+"""
 The Flask app settings
-'''
+"""
 
-import os
-from models import storage
-from flask import Flask, Blueprint
 from api.v1.views import app_views
+from flask import Blueprint, Flask
+from models import storage
+import os
 
 
 app = Flask(__name__)
 
 app.register_blueprint(app_views)
+
 
 @app.errorhandler(404)
 def not_found(error):
@@ -26,7 +27,7 @@ def close_session(exception):
 
 
 if __name__ == "__main__":
-    # port = environment variable HBNB_API_PORT or 5000 if not defined
-    # host = environment variable HBNB_API_HOST
     port = os.getenv('HBNB_API_PORT', 5000)
-    app.run(host='0.0.0.0', port=port, threaded=True, debug=True, use_reloader=False)
+    app.run(host='0.0.0.0',
+            port=port,
+            threaded=True)
