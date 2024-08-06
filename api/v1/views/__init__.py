9e9e321de53183d9bf9f8bdbde62c2a85d5f0034
Daniel Berthran <berthlearn@gmail.com>
2024-08-06 14:48:20 +0100

    uncomment state import

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -8,7 +8,7 @@ from flask import Blueprint
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 from api.v1.views.index import *
-# from views.states import *
+from api.v1.views.states import *
 # from views.cities import *
 # from views.amenities import *
 # from views.users import *
