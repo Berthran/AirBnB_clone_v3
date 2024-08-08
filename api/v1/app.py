4a388142fff7396552b39daf850d292c7605ec79
Daniel Berthran <berthlearn@gmail.com>
2024-08-08 03:45:12 +0100

    Remove debug option

--- api/v1/app.py
+++ api/v1/app.py
@@ -30,5 +30,4 @@ if __name__ == "__main__":
     port = os.getenv('HBNB_API_PORT', 5000)
     app.run(host='0.0.0.0',
             port=port,
-            threaded=True,
-            debug=True)
+            threaded=True)
