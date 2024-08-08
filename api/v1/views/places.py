84c75f5f480cd2701cdb93d47665d737b33079bd
Daniel Berthran <berthlearn@gmail.com>
2024-08-08 15:43:22 +0100

    Change route declaration

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -12,7 +12,7 @@ from models import storage
 from flask import jsonify, abort, request
 
 
-@app_views.get('/cities/<city_id>/places', strict_slashes=False)
+@app_views.route('/places', methods=['GET'], strict_slashes=False)
 def get_city_places(city_id):
     ''' Retrieves the list of all Place objects of a State '''
     city = storage.get(City, city_id)
@@ -22,7 +22,7 @@ def get_city_places(city_id):
     return jsonify(places)
 
 
-@app_views.get('/places/<place_id>', strict_slashes=False)
+@app_views.route('/places/<place_id>', methods=['GET'], strict_slashes=False)
 def get_place(place_id):
     ''' Retrieves a City object '''
     place = storage.get(Place, place_id)
@@ -32,7 +32,8 @@ def get_place(place_id):
         abort(404)
 
 
-@app_views.delete('/places/<place_id>', strict_slashes=False)
+@app_views.route('/places/<place_id>', methods=['DELETE'],
+                 strict_slashes=False)
 def delete_place(place_id):
     ''' Deletes a Place object '''
     place = storage.get(Place, place_id)
@@ -43,7 +44,7 @@ def delete_place(place_id):
     return jsonify({}), 200
 
 
-@app_views.post('/cities/<city_id>/places', strict_slashes=False)
+@app_views.route('/places', methods=['POST'], strict_slashes=False)
 def create_place(city_id):
     ''' Creates a Place Object '''
     city = storage.get(City, city_id)
@@ -63,7 +64,7 @@ def create_place(city_id):
     return jsonify(place.to_dict()), 201
 
 
-@app_views.put('/places/<place_id>', strict_slashes=False)
+@app_views.route('/places/<place_id>', methods=['PUT'], strict_slashes=False)
 def update_place(place_id):
     ''' Updates a Place Object '''
     place = storage.get(Place, place_id)
