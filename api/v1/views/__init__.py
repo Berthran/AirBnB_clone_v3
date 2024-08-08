3241674aee2b37a2b42554d3545df449763b1160
Daniel Berthran <berthlearn@gmail.com>
2024-08-08 04:10:57 +0100

    import cities

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -9,7 +9,7 @@ app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 from api.v1.views.index import *
 from api.v1.views.states import *
-# from views.cities import *
+from api.v1.views.cities import *
 # from views.amenities import *
 # from views.users import *
 # from views.places import *
