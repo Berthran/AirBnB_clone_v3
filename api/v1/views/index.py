b433c4c26ec204dda550643c642cde1da23cbc85
Daniel Berthran <berthlearn@gmail.com>
2024-08-08 03:39:48 +0100

    Rewrite index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,14 +5,14 @@ Create a route for status and stats
 
 from api.v1.views import app_views
 from flask import jsonify
-# from ....models import storage
-# from models import storage
-# from models.city import City
-# from models.amenity import Amenity
-# from models.user import User
-# from models.place import Place
-# from models.review import Review
-# from models.state import State
+from models import storage
+from models import storage
+from models.city import City
+from models.amenity import Amenity
+from models.user import User
+from models.place import Place
+from models.review import Review
+from models.state import State
 
 
 @app_views.route('/status')
@@ -21,12 +21,12 @@ def status():
     return jsonify({"status": "OK"})
 
 
-# @app_views.route('/stats', methods=['GET'], strict_slashes=False)
-# def stats():
-#     return jsonify({
-#             "amenities": storage.count(Amenity),
-#             "cities": storage.count(City),
-#             "places": storage.count(Place),
-#             "reviews": storage.count(Review),
-#             "states": storage.count(State),
-#             "users": storage.count(User)})
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stats():
+    return jsonify({
+            "amenities": storage.count(Amenity),
+            "cities": storage.count(City),
+            "places": storage.count(Place),
+            "reviews": storage.count(Review),
+            "states": storage.count(State),
+            "users": storage.count(User)})
